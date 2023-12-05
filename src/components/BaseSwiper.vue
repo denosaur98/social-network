e7efe70d24b176f838037ca27010917fc206846e
denosaur98 <densichurchill1998@gmail.com>
2023-12-06 00:50:24 +0300

    добавил библиотеку swiper

---
+++ src/components/BaseSwiper.vue
@@ -0,0 +1,54 @@
+<template>
+  <swiper
+    :slides-per-view="3"
+    :space-between="10"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+    loop="true"
+    class="swiper"
+  >
+    <swiper-slide class="slide">Slide 1</swiper-slide>
+    <swiper-slide class="slide">Slide 2</swiper-slide>
+    <swiper-slide class="slide">Slide 3</swiper-slide>
+    <swiper-slide class="slide">Slide 4</swiper-slide>
+    <swiper-slide class="slide">Slide 5</swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.swiper {
+  position: absolute;
+  width: 865px;
+  margin-top: 30px;
+  .slide {
+    padding: 5px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    width: 269.6px !important;
+  }
+}
+</style>  
--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="user-page">
     <h1 class="user-name" v-if="selectedUser">{{ props.selectedUser.name }}</h1>
+    <BaseSwiper/>
   </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
+import BaseSwiper from './BaseSwiper.vue';
 
 const props = defineProps(['selectedUser'])
 </script>
@@ -13,6 +15,7 @@ const props = defineProps(['selectedUser'])
 <style lang="scss" scoped>
 .user-page {
   display: flex;
+  flex-direction: column;
   border: 1px solid #000;
   border-radius: 5px;
   width: 100%;
